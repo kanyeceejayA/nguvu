ab3526c197cc8c7baf72e0d068fe86c87b3de75d
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2019-11-06 11:51:52 +0300

    done

--- pull.php
+++ pull.php
@@ -5,5 +5,14 @@
 if ( $_POST['payload'] ) {
 shell_exec( 'cd /python/nguvu/public_html/insights/ && git reset –hard HEAD && git pull' );
 }
+function isEnabled($func) {
+    return is_callable($func) && false === stripos(ini_get('disable_functions'), $func);
+}
+if (isEnabled('shell_exec')) {
+    shell_exec('echo "enabled"');
+    echo "it works, from echo functions";
+}else{
+	echo 'its not enabled';
+}
 
 ?>
