0b35ea81aeb51a50b471e10fd3403dd2ec243432
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2019-11-17 19:27:02 +0300

    fix easy login issue

--- admin/session.php
+++ admin/session.php
@@ -1,8 +1,8 @@
 <?php
-   	include('../actions/db_connection.php');
    	if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+   	include('../actions/db_connection.php');
 
    	if(!isset($_SESSION['username'])){
       
--- assets/header_admin.php
+++ assets/header_admin.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
 <?php require 'session.php';?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
