c4aa6073d786633b1aa7e58a03c9523262133bbd
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2019-12-24 11:28:28 +0300

    Update various queries.sql

--- actions/various queries.sql
+++ actions/various queries.sql
@@ -47,7 +47,7 @@ from startups
 LEFT JOIN type on startups.type_id=type.type_id
 LEFT JOIN type type2 on startups.type_id2=type2.type_id 
 LEFT JOIN status on startups.status_id = status.status_id
-LEFT JOIN (SELECT s_id,sum(amount) funding from deals group by s_id) d on d.s_id=startups.s_id
+LEFT JOIN (SELECT s_id,sum(amount) funding from sid_deals_amt_view group by s_id) d on d.s_id=startups.s_id
 LEFT JOIN (SELECT s_id,  GROUP_CONCAT(f_name) founders from founders GROUP by s_id) f on startups.s_id = f.s_id
 LEFT JOIN 
 	(select f.s_id, GROUP_CONCAT(c.name) as countries 
@@ -111,3 +111,16 @@ FROM deals d
 join startups s on d.s_id = s.s_id 
 JOIN investors i on d.inv_id = i.inv_id  
 GROUP by deal_id order by d_date desc
+
+
+CREATE VIEW `single_startups_view`  AS  
+select `startups`.`s_id` AS `s_id`,`startups`.`logo` AS `logo`,`startups`.`name` AS `name`,`type`.`type`
+ AS `type`,`type2`.`type` AS `type2`,`startups`.`email` AS `email`,`startups`.`funding_stage` 
+ AS `funding_stage`,`startups`.`employees` AS `employees`,`startups`.`description` 
+ AS `description`,`startups`.`location` AS `location`,`d`.`funding` 
+ AS `funding`,`startups`.`f_year` AS `f_year`,`status`.`status` AS `status`,`startups`.`website` 
+ AS `website`,`startups`.`facebook` AS `facebook`,`startups`.`twitter` AS `twitter`,`startups`.`linkedin` 
+ AS `linkedin`,`f`.`founders` AS `founders`,`c`.`countries` AS `countries` 
+ from ((((((`startups` left join `type` on(`startups`.`type_id` = `type`.`type_id`)) left join `type` `type2` on(`startups`.`type_id2` = `type2`.`type_id`)) left join `status` on(`startups`.`status_id` = `status`.`status_id`)) left join (select `deals`.`s_id` AS `s_id`,sum(`deals`.`amount`) AS `funding` from `deals` group by `deals`.`s_id`) `d` on(`d`.`s_id` = `startups`.`s_id`)) left join (select `founders`.`s_id` AS `s_id`,group_concat(`founders`.`f_name` separator ',') AS `founders` from `founders` group by `founders`.`s_id`) `f` on(`startups`.`s_id` = `f`.`s_id`)) left join (select `f`.`s_id` AS `s_id`,group_concat(`c`.`name` separator ',') AS `countries` from ((`startups` `s` join `c_of_operation` `f` on(`f`.`s_id` = `s`.`s_id`)) join `countries` `c` on(`c`.`country_id` = `f`.`c_id`)) group by `f`.`s_id`) `c` on(`c`.`s_id` = `startups`.`s_id`)) group by `startups`.`s_id` ;
+
+create view sid_deals_amt_view as SELECT DISTINCT s_id,deal_id, amount  from deals order by s_id
