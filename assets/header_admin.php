f0fcebbd4aa93488b8b43feb0404478f0ddb494f
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-01-27 13:57:13 +0300

    add g analytics

--- assets/header_admin.php
+++ assets/header_admin.php
@@ -17,8 +17,16 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
   <link href="../assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <!-- <link href="../assets/demo/demo.css" rel="stylesheet" /> -->
+
+  <script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-143871703-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
 </head>
 
 <body class="profile-page sidebar-collapse">
